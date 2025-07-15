3bc90af02bae7118accbba1a5a9c3e9a48dafcfe
cristian <cchura.cpc@gmail.com>
2025-07-14 20:41:13 -0400

    integrando template dashboard admin primeVue

---
+++ src/layout/AppFooter.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+    <div class="layout-footer">
+        Por
+        <a href="#" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">Nosotros</a>
+    </div>
+</template>
